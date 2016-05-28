8099ab4cdbc41564f72bf83d33c1f7d78ad6aa23
David Hantson <david.hantson@kbc.be>
2016-05-28 14:38:45 +0200

    Other pulse length

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ pwm = new Pca9685Driver(options, function() {
     console.log("Initialization done");
 });
 
+var channel = parseInt(process.argv[2]);
+var dutyCycle = parseFloat(process.argv[3]);
+
 // Set channel 0 to turn on on step 42 and off on step 255
 // pwm.setPulseRange(0, 42, 255);
 
 // Set the pulse length to 1500 microseconds for channel 2
-// pwm.setPulseLength(2, 1500);
+pwm.setPulseLength(2, 1700);
 
 // Set the duty cycle to 25% for channel 8
-pwm.setDutyCycle(0, 0.25);
+pwm.setDutyCycle(channel, dutyCycle);
+console.log(channel, dutyCycle);
